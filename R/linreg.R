eda32df0c81cfd48d01bece3875a4aa0a0d5ad01
chrillleee <christian.jonsson6@gmail.com>
2024-09-22 21:30:46 +0200

    Removed warnings in github actions

--- R/linreg.R
+++ R/linreg.R
@@ -1,3 +1,15 @@
+#' linreg
+#'
+#' @description This function implements different linjear regression algorithms.
+#' @param formula A list representing the adjacency list of the graph.
+#' @param data The starting node for the algorithm.
+#' @return A list containing the shortest paths from the source to all other nodes.
+#' @examples
+#' linreg(1, 2)
+#' 
+#' @references
+#' Wikipedia: \href{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}{Dijkstra's algorithm}
+#' @export
 linreg <-
 function(formula, data){
     print("hello")
