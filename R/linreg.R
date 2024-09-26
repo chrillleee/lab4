1245af908df5dc29fd05a43b2016a5fbb43f8b62
chrillleee <christian.jonsson6@gmail.com>
2024-09-26 23:02:51 +0200

    Finished 1.1, 1.2, 1.4

--- R/linreg.R
+++ R/linreg.R
@@ -10,7 +10,84 @@
 #' @references
 #' Wikipedia: \href{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}{Dijkstra's algorithm}
 #' @export
+#' 
 
-linreg <-function(formula, data){
-  return(linregClass$new(formula, data))
-}
+linreg <- setRefClass(
+  "linreg",
+  fields = list(
+    formula = "formula",
+    data = "data.frame",
+    X = "matrix",
+    y = "character",
+    regressionCoeff = "matrix",
+    fittedValues = "matrix",
+    residuals = "matrix",
+    dof = "numeric",
+    residualVariance = "numeric",
+    regressionCoefficientsVariance = "matrix",
+    tValues = "matrix",
+    cumulativeDistribution  = "matrix"
+  ),
+  methods = list(
+    # constructor
+    initialize = function(formula = NA, data = NA) {
+      stopifnot(rlang::is_formula(formula))
+      stopifnot(is.data.frame(data))
+      .self$formula <- formula  
+      .self$data <- data 
+      .self$X <- model.matrix(formula, data)  
+      .self$y <- all.vars(formula)[1]
+      .self$calculateRegressionCoeff()
+      .self$calculateFittedValues()
+      .self$calculateResiduals()
+      .self$calculateDegreesOfFreedom()
+      .self$calculateResidualVariance()
+      .self$calculateRegressionCoefficientsVariance()
+      .self$calculateTDistribution()
+      .self$calculateCumulativeDistribution()
+
+    },  
+    printMembers = function() {
+      cat("Matrix X:\n")
+      base::print(.self$cumulativeDistribution)
+    },
+    calculateRegressionCoeff = function() {
+      transposeX = t(.self$X) 
+      X_inverse <- solve(transposeX %*% .self$X)
+      .self$regressionCoeff <- X_inverse %*% transposeX %*% .self$data[[.self$y ]]
+    },
+    calculateFittedValues = function() {
+      .self$fittedValues <- .self$X %*% regressionCoeff
+    },
+    calculateResiduals= function() {
+      .self$residuals <- .self$data[[.self$y]] - fittedValues
+    },
+    calculateDegreesOfFreedom = function(){
+      n_observations <- nrow(.self$data)
+      p_parameters<- ncol(.self$X) - 1  # -1 for the intercept
+      .self$dof <- n_observations - p_parameters
+    },
+    calculateResidualVariance = function(){
+      .self$residualVariance <- as.numeric(t(.self$residuals) %*% .self$residuals / .self$dof)
+    },
+    calculateRegressionCoefficientsVariance = function(){
+      transposeX = t(.self$X) 
+      X_inverse <- solve(transposeX %*% .self$X)
+      .self$regressionCoefficientsVariance <- .self$residualVariance * solve(transposeX %*% .self$X)
+    },
+    calculateTDistribution = function(){
+      # .self$tValues <- regressionCoeff %*% solve(expm::sqrtm(.self$regressionCoefficientsVariance)) %*% regressionCoeff
+      standardErrors <- sqrt(diag(.self$regressionCoefficientsVariance))
+      .self$tValues <- .self$regressionCoeff / standardErrors
+    },
+    calculateCumulativeDistribution = function(){
+        .self$cumulativeDistribution <- pt(.self$tValues, .self$dof)
+    },
+    print = function() {},
+    plot = function() {},
+    resid = function() {},
+    pred = function() {},
+    coef = function() {},
+    summary = function() {}
+  )
+)
